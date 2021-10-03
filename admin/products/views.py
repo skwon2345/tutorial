901c4d9f0aad385ce78bf94c634af6664af615b6
sukkwono-hits <sukkwono@hits.ai>
2021-10-03 22:17:30 +0900

    products: views: add ProductViewSet

--- admin/products/views.py
+++ admin/products/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+import random
 
-# Create your views here.
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .models import Product, User
+from .serializers import ProductSerializer
+
+
+class ProductViewSet(viewsets.ViewSet):
+    def list(self, request):  # /api/products
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+
+        return Response(serializer.data)
+
+    def create(self, request):  # /api/products
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def retrieve(self, request, pk=None):  # /api/products/<str:id>
+        product = Product.objects.get(id=pk)
+        serializer = ProductSerializer(product)
+
+        return Response(serializer.data)
+
+    def update(self, request, pk=None):  # /api/products/<str:id>
+        product = Product.objects.get(id=pk)
+        serializer = ProductSerializer(
+            instance=product, data=request.data
+        )  # instance is the product in db, and data is user want to update.
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+
+    def destroy(self, request, pk=None):  # /api/products/<str:id>
+        product = Product.objects.get(id=pk)
+        product.delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
