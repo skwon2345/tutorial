533045b2c20c45b0fed3a5d4af0d93945d0158b2
sukkwono-hits <sukkwono@hits.ai>
2021-10-17 11:13:35 +0900

    producer: add producer to connect to admin container through rabbitMQ

---
+++ main/producer.py
@@ -0,0 +1,17 @@
+import json
+
+import pika  # package to send events to RabbitMQ
+
+params = pika.URLParameters(
+    "amqps://uvznzatz:0hrHHWQvv-jfx31b-VHKsz8ie7GfGqjM@dingo.rmq.cloudamqp.com/uvznzatz"
+)
+
+connection = pika.BlockingConnection(params)
+
+channel = connection.channel()
+
+
+def publish(method, body):
+    properties = pika.BasicProperties(method)
+    channel.basic_publish(exchange="", routing_key="admin", body=json.dumps(body), properties=properties)
+
